3333da23828b75a3d8eb7e6d99d298c7a16f3288
ujjawal <ujjawalrachhoya100@gmail.com>
2022-06-11 00:30:26 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
 
 function App() {
+  const [input, setinput] = useState("")
+  const onchange = (e) => {
+    setinput(e.target.value)
+    console.log(input)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <input onChange={onchange} value={input} name="" id="" cols="30" rows="2"></input>
+      <a href={`https://source.unsplash.com/random/900x700/?${input}`} target="_blank">Submit</a>
+    </>
+  )
 }
 
 export default App;
